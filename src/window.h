775f72f4cf93994eae3ae6bb107547c7ac23ce8d
S <ricardoliang@gmail.com>
2013-05-09 17:37:44 -0300

    Added window.* files

---
+++ src/window.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+
+#ifndef XCBWINDOW_H
+#define XCBWINDOW_H
+
+#include <xcb/xcb.h>
+#include <stdbool.h>
+
+struct xcbwindow {
+	xcb_connection_t *connection;
+	xcb_visualtype_t *visualtype;
+	xcb_window_t window;
+	uint16_t window_width;
+	uint16_t window_height;
+};
+
+extern struct xcbwindow xcb_window;
+
+void xcbwindow_init(uint16_t height, bool on_bottom);
+void xcbwindow_flush();
+void xcbwindow_terminate();
+
+#endif /* XCBWINDOW_H */
