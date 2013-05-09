f90c2a94e618fe0a206f227df4b778064cbf7299
S <ricardoliang@gmail.com>
2013-05-09 16:30:07 -0300

    Added init function

--- src/main.c
+++ src/main.c
@@ -8,6 +8,25 @@
 #include "xcbwindow.h"
 #include "engine.h"
 
+bool init(int argc, char *argv[])
+{
+	options_init(argc, argv);
+	xcbwindow_init(options.bar_height, options.bar_on_bottom);
+
+	int window_width = xcb_window.window_width;
+	int window_height = options.bar_height;
+
+	cairo_surface_t *surface = cairo_xcb_surface_create(
+			xcb_window.connection, xcb_window.window,
+			xcb_window.visualtype, window_width, window_height);
+
+	engine_init_canvas(surface, window_width, window_height,
+			options.default_foreground, options.default_foreground);
+	engine_init_sets(options.sizes, options.default_font);
+
+	xcbwindow_flush();
+}
+
 int main(int argc, char *argv[])
 {
 	options_init(argc, argv);
