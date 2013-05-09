292bacbf45c5761162b21471b2fcf42b895c6dfc
S <ricardoliang@gmail.com>
2013-05-09 17:03:07 -0300

    Replaced some xcbwindow_init code with create_window call.

--- src/xcbwindow.c
+++ src/xcbwindow.c
@@ -35,22 +35,19 @@ static void create_window(xcb_screen_t *s, int16_t x, int16_t y)
 void xcbwindow_init(uint16_t height, bool on_bottom)
 {
 	xcb_window.connection = xcb_connect(NULL, NULL);
+
 	xcb_screen_t *s = xcb_setup_roots_iterator(
 			xcb_get_setup(xcb_window.connection)).data;
 	xcb_window.visualtype = get_root_visual_type(s);
+
 	xcb_window.window = xcb_generate_id(xcb_window.connection);
 	xcb_window.window_width = s->width_in_pixels;
 	xcb_window.window_height = height;
-	xcb_create_window(xcb_window.connection, XCB_COPY_FROM_PARENT,
-			xcb_window.window, s->root,
-			0, on_bottom ? s->height_in_pixels - height : 0,
-			xcb_window.window_width, height,
-			0, XCB_WINDOW_CLASS_INPUT_OUTPUT,
-			s->root_visual, XCB_CW_EVENT_MASK,
-			(const uint32_t []){ [0] = XCB_EVENT_MASK_EXPOSURE });
-	xcb_change_window_attributes(xcb_window.connection, xcb_window.window,
-			XCB_CW_OVERRIDE_REDIRECT, (const uint32_t []){ 1 });
-	xcb_map_window(xcb_window.connection, xcb_window.window);
+
+	int16_t window_x = 0;
+	int16_t window_y = on_bottom ? s->height_in_pixels - height : 0;
+
+	create_window(s, window_x, window_y);
 }
 
 void xcbwindow_flush()
