13c21a8687c05ca34a7a3cd58204ddec63448679
S <ricardoliang@gmail.com>
2013-05-09 16:37:03 -0300

    Replaced main with init, main_loop and terminate functions.

--- src/main.c
+++ src/main.c
@@ -51,26 +51,7 @@ void terminate()
 
 int main(int argc, char *argv[])
 {
-	options_init(argc, argv);
-	xcbwindow_init(options.bar_height, options.bar_on_bottom);
-	int window_width = xcb_window.window_width;
-	int window_height = options.bar_height;
-	cairo_surface_t *surface = cairo_xcb_surface_create(
-			xcb_window.connection, xcb_window.window,
-			xcb_window.visualtype, window_width, window_height);
-	engine_init_canvas(surface, window_width, window_height,
-			options.default_foreground, options.default_foreground);
-	engine_init_sets(options.sizes, options.default_font);
-	xcbwindow_flush();
-	int id;
-	char buffer[BUFSIZ];
-	for (;;) {
-		scanf("%d %s", &id, buffer);
-		engine_update(buffer, id);
-		xcbwindow_flush();
-	}
-	options_terminate();
-	engine_terminate();
-	xcbwindow_terminate();
-	return 0;
+	if (init(argc, argv))
+		main_loop();
+	terminate();
 }
