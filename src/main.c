da45ec98ce6c364d9867e6eda7d06ce6f7ac4a29
S <ricardoliang@gmail.com>
2013-05-21 15:07:30 -0300

    Removed unneeded includes.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 #include <poll.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <cairo-xcb.h>
 
