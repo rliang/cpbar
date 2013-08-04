754143719532f9ea75353b16a88b344ddb93cd9e
S <ricardoliang@gmail.com>
2013-08-04 13:34:08 -0300

    Updated LICENSE, included in all sources.

--- src/options.c
+++ src/options.c
@@ -1,4 +1,24 @@
-/* See LICENSE file for copyright and license details. */
+/*
+ * Copyright (C) 2013 Ricardo Liang <ricardoliang@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #include "options.h"
 
@@ -111,8 +131,10 @@ void options_destroy(struct options *options)
 		options->default_background
 	};
 
-	for (int i = 0; i < 3; ++i)
-		free(strings[i]);
+	/*
+	 *for (int i = 0; i < 3; ++i)
+	 *        free(strings[i]);
+	 */
 
 	free(options);
 }
